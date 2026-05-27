a6cb1a8c7f1a2b63fb2c18e74b534ab2b9be570f
JustinDev26 <justinfoxy12@gmail.com>
2026-05-27 11:03:53 -0600

    Add files via upload

---
+++ english_trainer.py
@@ -0,0 +1,8 @@
+#CREATE A PHRASE
+english_phrase="i want to grow in this company"
+print("traduce esto al ingles:Quiero crecer en esta empresa")
+my_answer=input("your answer: ")
+if my_answer== english_phrase:
+    print("excellent bro🔥! the chunk is perfect.")
+else:
+    print("close but no cigar🚬")
